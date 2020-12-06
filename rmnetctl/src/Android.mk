0b0df1fab98d65e3c2cdb5909e0a9f9bec719de6
spezi77 <spezi7713@gmx.net>
2020-12-06 10:10:53 +0100

    librmnetctl: Use qti kernel headers.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- rmnetctl/src/Android.mk
+++ rmnetctl/src/Android.mk
@@ -6,4 +6,6 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_COPY_HEADERS_TO   := dataservices/rmnetctl
 LOCAL_COPY_HEADERS      := ../inc/librmnetctl.h
 
+LOCAL_HEADER_LIBRARIES := qti_kernel_headers
+
 include $(BUILD_COPY_HEADERS)
